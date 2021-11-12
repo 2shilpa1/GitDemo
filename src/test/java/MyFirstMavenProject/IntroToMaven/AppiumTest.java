986b05a15892c04915d89203dcbf30917b7a0437
shilpa <shilpa.namdeo21@gmail.com>
2021-11-12 12:24:15 +0530

    first commit

---
+++ src/test/java/MyFirstMavenProject/IntroToMaven/AppiumTest.java
@@ -0,0 +1,16 @@
+package MyFirstMavenProject.IntroToMaven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void  demoAppium()
+	 {
+		 System.out.println("I am from Appium Demo");
+	 }
+	@Test
+	public void  SetAppium()
+	 {
+		 System.out.println("I am from Set Appium");
+	 }
+}
---
+++ src/test/java/MyFirstMavenProject/IntroToMaven/SeleniumTest.java
@@ -0,0 +1,17 @@
+package MyFirstMavenProject.IntroToMaven;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+ public void  demoSelenium()
+ {
+	 System.out.println("I am from Selenium Demo");
+ }
+ @Test
+ public void  SetSelenium()
+ {
+	 System.out.println("I am from Set Selenium");
+ }
+ 
+}
